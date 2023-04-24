477251085d01a0cad54a5a2873678a319f45a2ff
mohammed yaseen <mhdyaseen.official@gmail.com>
2023-04-24 22:26:28 +0530

    doc management added

---
+++ src/pages/DashboardAppPage.js
@@ -0,0 +1,59 @@
+import { Helmet } from 'react-helmet-async';
+import { faker } from '@faker-js/faker';
+// @mui
+import { useTheme } from '@mui/material/styles';
+import { Grid, Container, Typography } from '@mui/material';
+// components
+import Iconify from '../components/iconify';
+// sections
+import {
+  AppTasks,
+  AppNewsUpdate,
+  AppOrderTimeline,
+  AppCurrentVisits,
+  AppWebsiteVisits,
+  AppTrafficBySite,
+  AppWidgetSummary,
+  AppCurrentSubject,
+  AppConversionRates,
+} from '../sections/@dashboard/app';
+
+// ----------------------------------------------------------------------
+
+export default function DashboardAppPage() {
+  const theme = useTheme();
+
+  return (
+    <>
+      <Helmet>
+        <title> Dashboard | HSTORY </title>
+      </Helmet>
+
+      <Container maxWidth="xl">
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          Hi, Welcome back
+        </Typography>
+
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary title="Weekly Sales" total={714000} icon={'ant-design:android-filled'} />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary title="New Users" total={1352831} color="info" icon={'ant-design:apple-filled'} />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary title="Item Orders" total={1723315} color="warning" icon={'ant-design:windows-filled'} />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary title="Bug Reports" total={234} color="error" icon={'ant-design:bug-filled'} />
+          </Grid>
+
+        
+        </Grid>
+      </Container>
+    </>
+  );
+}
